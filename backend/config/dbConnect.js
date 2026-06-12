33233c0d46e97e00be033bf4cb378ca3ad81118d
thuan020104 <thuan_2251220047@dau.edu.vn>
2026-06-12 10:41:38 +0700

    Fix connection data

--- backend/config/dbConnect.js
+++ backend/config/dbConnect.js
@@ -5,7 +5,7 @@ const {
 const dbConnect = async () => {
   try {
     await mongoose.connect(
-      process.env.MONGO_URI || "mongodb://localhost:27017/manager-btaskee"
+      process.env.MONGO_URI || "mongodb+srv://thuan2251220047_db_user:snKYur2AQ1dw5Dph@cluster0.wqmixy5.mongodb.net/?appName=Cluster0"
     );
     console.log("✓ Database connected successfully");
   } catch (error) {
