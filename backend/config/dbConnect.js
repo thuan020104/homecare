1ab6069d28459e7498c7e91f9b89157bfcf5ab59
thuan020104 <thuan_2251220047@dau.edu.vn>
2026-06-19 09:34:20 +0700

    Fix PORT variable

--- backend/config/dbConnect.js
+++ backend/config/dbConnect.js
@@ -5,7 +5,7 @@ const {
 const dbConnect = async () => {
   try {
     await mongoose.connect(
-      process.env.MONGO_URI || "mongodb+srv://thuan2251220047_db_user:snKYur2AQ1dw5Dph@cluster0.wqmixy5.mongodb.net/?appName=Cluster0"
+      process.env.MONGO_URI || "mongodb+srv://thuan2251220047_db_user:<db_password>@cluster0.wqmixy5.mongodb.net/<Cluster0>"
     );
     console.log("✓ Database connected successfully");
   } catch (error) {
--- backend/routers/serviceRoutes.js
+++ backend/routers/serviceRoutes.js
@@ -3,13 +3,14 @@ const router = express.Router();
 const serviceController = require("../controllers/serviceController");
 
 // CRUD dịch vụ
-router.get("/getall", serviceController.getAllServices);
-router.get("/get/:id", serviceController.getServiceById);
+router.get("/", serviceController.getAllServices);
+router.get("/:id", serviceController.getServiceById);
 
 router.get("/:serviceId/reviews", serviceController.getReviewsForService);
 router.post("/:serviceId/reviews", serviceController.addReviewToService);
-router.post("/create", serviceController.createService);
-router.put("/update/:id", serviceController.updateService);
-router.delete("/delete/:id", serviceController.deleteService);
+
+router.post("/", serviceController.createService);
+router.put("/:id", serviceController.updateService);
+router.delete("/:id", serviceController.deleteService);
 
 module.exports = router;
